7ad73a4938a86ddf052c71776f357da441e842e8
JoVic-C <86211332+JoVic-C@users.noreply.github.com>
2021-09-16 15:15:56 -0300

    Add files via upload

---
+++ Aula 1 - Calculadora/calculator.js
@@ -0,0 +1,24 @@
+// Declaração de Variaveis
+
+var notaPrimeiroBimestre = 1
+var notaSegundoBimestre = 1
+var notaTerceiroBimestre = 1
+
+// Função para calculo de média
+var media = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre) / 3
+
+// console.log é o imprime as coisas para o terminal, como um printf e tals
+console.log(media)
+
+// Aqui funciona como o If/Else do Java
+
+// Se a media for maior que 7, o aluno é aprovado
+if (media > 7) 
+{
+    console.log('O aluno foi aprovado.');
+}
+
+// Se a media for menor que 7, o aluno é reprovado
+if (media < 7) {
+    console.log('O aluno foi reprovado.');
+}    
