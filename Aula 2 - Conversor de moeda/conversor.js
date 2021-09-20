2cb1bc0a56045ab3b63138627a78f6e15445c51c
JoVic-C <jo.joaovictor828@gmail.com>
2021-09-20 04:16:53 -0300

    Criação de outros convertores

--- Aula 2 - Conversor de moeda/conversor.js
+++ Aula 2 - Conversor de moeda/conversor.js
@@ -1,21 +1,59 @@
 // Criação de uma função
-function Converter() {
+function ConverterEmDolar() {
     // getElementById é uma função que vai buscar no HTML o id valor
     var valorElemento = document.getElementById("valor");
     var valor = valorElemento.value;
 
     // parseFloat é um comando do Javascript que transforma uma variavel em float, que seria o double do Java
-    var valorEmDolarNumerico = parseFloat(valor);
-    
-    // Conversão do dolar em real
-    var valorEmReal = valorEmDolarNumerico * 5;
-    console.log(valorEmReal);
-    var elementoValorConvertido = document.getElementById("valorConvertido");
+    var valorNumerico = parseFloat(valor);
+    // Conversão do real em dolar   
+    var valorEmDolar = valorNumerico * 5.29;
+    console.log(valorEmDolar);
+    var elementoValorConvertido = document.getElementById("valorConvertido")
 
     // Criação de uma variavel de texto
-    var valorConvertido = "Resultado em real é R$ " +  valorEmReal;
+    var valorConvertido = "Convertendo "+ valorNumerico + " reais em dolar o resultado é: " +  valorEmDolar.toFixed(2) + " dolares";
 
     // innerHTML é um comando que colocar a variavel no HTML, inner= interno/dentro
-    elementoValorConvertido.innerHTML = valorConvertido;
+    elementoValorConvertido.innerHTML = valorConvertido;  
+}
+/*----------------------------------------MELHORIA DE COGIDO-------------------------------------------------------------------------------------*/
+
+function ConverterEmEuro () {
+  var valorElemento = document.getElementById("valor");
+  var valor = valorElemento.value;
+  var valorNumerico = parseFloat(valor);  
   
-  }
+  var valorEmEuro = valorNumerico * 6.20 ;
+  console.log(valorEmEuro);
+  var elementoValorConvertido = document.getElementById("valorConvertido") 
+  var valorConvertido = "Convertendo "+ valorNumerico + " reais em dolar o resultado é: " +  valorEmEuro.toFixed(2) + " euros";
+
+  elementoValorConvertido.innerHTML = valorConvertido; 
+}
+
+function ConverterEmBitcoin() {
+  var valorElemento = document.getElementById("valor");
+  var valor = valorElemento.value;
+  var valorNumerico = parseFloat(valor);  
+  
+  var valorEmBitcoin = valorNumerico * 241363.98 ;
+  console.log(valorEmBitcoin);
+  var elementoValorConvertido = document.getElementById("valorConvertido") 
+  var valorConvertido = "Convertendo "+ valorNumerico + " reais em dolar o resultado é: " +  valorEmBitcoin.toFixed(2) + " bitcoin";
+
+  elementoValorConvertido.innerHTML = valorConvertido; 
+}
+
+function ConverterEmLibra() {
+  var valorElemento = document.getElementById("valor");
+  var valor = valorElemento.value;
+  var valorNumerico = parseFloat(valor);  
+  
+  var valorEmLibra = valorNumerico * 7.24 ;
+  console.log(valorEmLibra);
+  var elementoValorConvertido = document.getElementById("valorConvertido") 
+  var valorConvertido = "Convertendo "+ valorNumerico + " reais em dolar o resultado é: " +  valorEmLibra.toFixed(2) + " libras";
+
+  elementoValorConvertido.innerHTML = valorConvertido; 
+}
